f1a093b8c7dcc8228ff1e03931d0879b521225e4
milkslurpee <johnsuorsa@gmail.com>
2025-11-22 00:16:11 -0700

    added project 6

---
+++ Project6/test_utils.py
@@ -0,0 +1,20 @@
+import math
+
+from utils import score_tour
+
+
+def assert_valid_tour(edges, tour):
+    """
+    Length is number of vertices
+    Not vertices repeated
+    Non-infinite score
+    """
+    assert len(tour) == len(edges)
+    assert len(tour) == len(set(tour))
+    assert not math.isinf(score_tour(tour, edges))
+
+
+def assert_valid_tours(edges, stats):
+    assert len(stats) > 0
+    for stat in stats:
+        assert_valid_tour(edges, stat.tour)
